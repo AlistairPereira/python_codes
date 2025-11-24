a3ddee6e3c6a507c443201bbc747bf462040140e
AlistairPereira <alistairpereira241999@gmail.com>
2025-11-24 02:12:56 +0100

    dictionaries

--- codes/dictionaries.py
+++ codes/dictionaries.py
@@ -74,3 +74,54 @@ print(name.strip())
 name = "###Alistair###"
 print(name.strip("#"))
 print(name.lstrip("#"))
+
+
+
+squares = {x: x*x for x in range(1,6)}
+print(squares)
+
+# Using items() — Copy a dictionary
+d = {"a":1, "b":2, "c":3}
+copy_d = {k:v for k,v in d.items()}
+print(copy_d)
+
+#Convert all values to uppercase
+fruits = {"a": "apple", "b": "banana", "c": "cherry"}
+x ={k:v.upper() for k,v in fruits.items() }
+print(x)
+
+# Keep only items where value > 2
+nums = {"a":1, "b":5, "c":2, "d":7}
+x= {k:v for k,v in nums.items() if v > 2}
+print(x)
+
+#Swapping Keys and Values (VERY useful)
+d = {"a":1, "b":2, "c":3}
+res ={v:k for k,v in d.items()}
+print(res)
+
+# Frequency Count Using Dict Comprehension + set()
+# We can compress it using comprehension:
+
+sentence = "this is a test this is only a test"
+words = sentence.split()
+# print(words)
+d={}
+for w in words:
+    if w in d:
+        d[w] +=1
+    else:
+        d[w] =1
+print(d)
+    
+    
+res = {x: ("even" if x % 2 == 0 else "odd") for x in range(1,11)}
+print(res)
+
+res = {x: ("even" if x%2==0 else "odd") for x in range(1,11)}
+print(res)
+
+sentence = "this is a test this is only a test"
+words = sentence.split()
+freq = {w: words.count(w) for w in set(words)}
+print(freq)
